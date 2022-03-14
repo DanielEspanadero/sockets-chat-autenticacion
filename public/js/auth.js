6b96bd1ed3759bdc9f8051fc9793f3b8e5423d02
Daniel Españadero <developer@daniel-espanadero.com>
2022-03-14 23:15:12 +0100

    token added at localstorage

--- public/js/auth.js
+++ public/js/auth.js
@@ -6,11 +6,11 @@ var url = (window.location.hostname.includes('localhost'))
 
 function onSignIn(googleUser) {
 
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    // var profile = googleUser.getBasicProfile();
+    // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    // console.log('Name: ' + profile.getName());
+    // console.log('Image URL: ' + profile.getImageUrl());
+    // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 
     var id_token = googleUser.getAuthResponse().id_token;
     const data = { id_token };
@@ -22,7 +22,7 @@ function onSignIn(googleUser) {
     })
         .then(resp => resp.json())
         .then(({ token }) => {
-            console.log(token)
+            localStorage.setItem(token);
         })
         .catch(console.log);
 
